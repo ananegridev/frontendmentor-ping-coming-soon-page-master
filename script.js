e8aa7e7923f825a75d375dacb601a3a5da249b64
Ana Paula Negri <85410129+ananegridev@users.noreply.github.com>
2022-09-21 15:22:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
-const email = document.getElementById("email");
+const bordaEmail = document.getElementById("email");
 const btn = document.getElementById("button");
 const error = document.getElementById("error");
 
-btn.addEventListener("click", function() {
-	const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-	if (email.value.match(validRegex)) {
-		error.style.display = "none";
-		// email.style.borderColor = "hsl(223, 100%, 88%)";
-		// email.style.fontFamily = "'Libre Franklin', sans-serif";
-		// email.style.color = "hsl(209, 33%, 12%)";
-	} else {
-		error.style.display = "flex";
-		email.style.borderColor = "hsl(354, 100%, 66%)";
-		// email.style.fontFamily = "'Libre Franklin', sans-serif";
-		email.style.color = "hsl(209, 33%, 12%)";
-	}
-})
-
-
-
-
+document.getElementById("email").onkeyup = function () {
+  const validRegex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (validRegex.test(this.value)) {
+    error.style.display = "none";
+    bordaEmail.style.borderColor = "green";
+  } else {
+    error.style.display = "flex";
+    bordaEmail.style.borderColor = "hsl(354, 100%, 66%)";
+  }
+};
